9270f5d4ebd885ab28a85568b4ec94a25abb4c82
BoppanaSandeep <boppanasandeep57@gmail.com>
2019-03-10 16:43:59 +0530

    dyanmic years and months

--- profile/src/app/app.component.ts
+++ profile/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import * as moment from 'moment';
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = "app";
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    this.noOfYearsExp(1);
+  }
+
+  noOfYearsExp(type) {
+    const now = moment(new Date()); // todays date
+    const end = moment('2016-06-28'); // another date
+    const duration = moment.duration(now.diff(end));
+    let days = duration.asYears();
+    days = Number(days.toFixed(1));
+    const exp = String(days).split('.');
+    // console.log(exp[type], exp);
+    return exp[type] = exp[type] ? exp[type] : '0'; // type 0 - years, 1 - months
+  }
 }
